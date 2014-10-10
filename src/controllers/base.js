d09eae3c9b524d79a0c3116d984a07d9abdfcedc
Guo Yu <i@guoyu.me>
2014-10-11 01:47:21 +0800

    0.1: Add lib auth0

--- src/controllers/base.js
+++ src/controllers/base.js
@@ -10,22 +10,22 @@
     ]);
   
   function baseCtrler($scope, $state, $timeout, $location) {
-    // inject locals to template
+    // Inject locals to template
     $scope.location = $location;
     $scope.state = $state;
-    // signin section
-    $scope.hiddenSigninSection = true;
-    $scope.toggleSigninSection = toggleSigninSection;
-    // alerts module
+    $scope.copyrightYear = (new Date()).getFullYear();
+    
+    /**
+    *
+    * Alert utils
+    * @example
+    *   addAlert();
+    *   closeAlert();
+    *
+    **/
     $scope.alerts = [];
     $scope.addAlert = addAlert;
     $scope.closeAlert = closeAlert;;
-    // init copyright
-    $scope.copyrightYear = (new Date()).getFullYear();
-
-    function toggleSigninSection() {
-      $scope.hiddenSigninSection = !$scope.hiddenSigninSection;
-    }
 
     function addAlert(msg, type, dismiss) {
       $scope.alerts.push({
@@ -38,7 +38,6 @@
       }, dismiss ? (dismiss * 1000) : 3000);
       return alertIndex;
     }
-
     function closeAlert(index) {
       $scope.alerts.splice(index, 1);
     }
